76be5d986b6e5869584820a308ca08ebac37b18c
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-22 09:43:51 +0200

    Initial commit

---
+++ src/main/java/inc/sims/hustles/JobPortalApiApplication.java
@@ -0,0 +1,13 @@
+package inc.sims.hustles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalApiApplication.class, args);
+	}
+
+}
