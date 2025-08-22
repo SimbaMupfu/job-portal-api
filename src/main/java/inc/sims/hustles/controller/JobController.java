d2109be50ca9e6f3d89b60f12fe028e493e2710b
SimbaMupfu <scmupfururirwa@gmail.com>
2025-08-22 10:31:00 +0200

    Use @RestController to remove the need to add @ResponseBody on each method

--- src/main/java/inc/sims/hustles/controller/JobController.java
+++ src/main/java/inc/sims/hustles/controller/JobController.java
@@ -3,20 +3,18 @@ package inc.sims.hustles.controller;
 import inc.sims.hustles.model.JobPost;
 import inc.sims.hustles.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class JobController {
 
     @Autowired
     private JobService jobService;
 
     @GetMapping("job_posts")
-    @ResponseBody
     public List<JobPost> getAllJobs(){
         return jobService.getAllJobs();
     }
